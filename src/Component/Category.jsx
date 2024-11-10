c219522b6301163565887552be8edcac730ab32f
Rushikesh parekar <148846308+Rushi-Parekar11@users.noreply.github.com>
2024-11-10 23:48:18 +0530

    Update Category.jsx

--- src/Component/Category.jsx
+++ src/Component/Category.jsx
@@ -15,9 +15,9 @@ function Category({GetCategory,mood}) {
     <div className="btngroup" style={mood ? {backgroundColor:' #f4f4f4'}:{backgroundColor:'#18181b'}} >
         <span className='spancat'  style={coloron===1 ?{backgroundColor: '#ef4440',color:'white'}:{} } onClick={()=>{setcoloron(1);GetCategory("All")}} >All</span>
         <span className='spancat'  style={coloron===2 ?{backgroundColor: '#ef4440',color:'white'}:{} } onClick={()=>{setcoloron(2);GetCategory("Lunch")}}  >Lunch</span>
-        <span className='spancat'  style={coloron===3 ?{backgroundColor: '#ef4440',color:'white'}:{} } onClick={()=>{setcoloron(3);GetCategory("Breakfast")}} >Breakfast</span>
+        <span className='spancat'    style={coloron===3 ?{backgroundColor: '#ef4440', color:'white'}:{} } onClick={()=>{setcoloron(3);GetCategory("Breakfast")}} >Breakfast</span>
         <span className='spancat'  style={coloron===4 ?{backgroundColor: '#ef4440',color:'white'}:{} }  onClick={()=>{setcoloron(4);GetCategory("Dinner")}} >Dinner</span>
-        <span className='spancat'  style={coloron===5 ?{backgroundColor: '#ef4440',color:'white'}:{} }  onClick={()=>{setcoloron(5);GetCategory("Snacks")}} >Snacks</span>
+        <span className='spancat'    style={coloron===5 ?{backgroundColor: '#ef4440',color:'white'}:{} }  onClick={()=>{setcoloron(5);GetCategory("Snacks")}} >Snacks</span>
     </div>
 
     </div>
